93f2a12ddde04454a82ead66fcf7a88aabc3ab76
Yunyun <celeph4is@gmail.com>
2020-04-17 15:13:47 -0700

    display names in persons array as a list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
+  const [ persons, setPersons] = useState([ // person is an object!!!
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
   return (
     <div>
+      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
       <form>
         <div>
@@ -18,9 +19,16 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+     {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
 
 export default App
+
+// get app to render persons state
+
+// add names to list: 
+//1. text input will have an onChange handler to set user input to newName state
+//2. button element will have an onClick handler that adds the value in newName //into the persons array
+// array.map() used to render array of names
