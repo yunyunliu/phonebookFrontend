6b8d6268344d472effe8b8e0313af5be45af5290
Yunyun <celeph4is@gmail.com>
2020-04-24 09:43:08 -0700

    change backend url to relative url becuse both frontend and backend at the same address

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'https://fierce-waters-14148.herokuapp.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
